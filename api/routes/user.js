f3ca53eb6da7050f9fb80f0886282822124d0c5f
mitarcon <mitarcon@gmail.com>
2017-12-21 23:47:33 -0500

    controlador de guardar usuario

--- api/routes/user.js
+++ api/routes/user.js
@@ -6,6 +6,7 @@ var UserController = require('../controllers/user');
 var api = express.Router();
 
 api.get('/user', UserController.home);
+api.post('/user', UserController.saveUser);
 
 
 module.exports = api;
