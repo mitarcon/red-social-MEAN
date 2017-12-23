855e2d7ac6325786df12bc5c44cf10e3c232b8ab
mitarcon <mitarcon@gmail.com>
2017-12-23 15:22:18 -0500

    agregar i18n

--- api/app.js
+++ api/app.js
@@ -2,6 +2,8 @@
 
 var express = require('express');
 var bodyParser = require('body-parser');
+var i18n = require('i18n');
+
 
 var app = express();
 
@@ -22,11 +24,23 @@ app.use(
 );
 
 
-//cors
 
+//cors
 
 //rutas
 app.use('/api', user_routes);
+
+//Internacionalization
+i18n.configure({
+    locales:['es', 'en'],
+    directory: __dirname + '/locales',
+    defaultLocale: 'es'
+});
+
+//init i18n after cookie-parser
+app.use(i18n.init);
+console.log("--- ",i18n.__('hello'));
+
 
 //exportar
 module.exports = app;
