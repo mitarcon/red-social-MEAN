a177fcd3bd3bb80fc4036fa8782d32987807b1ad
mitarcon <mitarcon@gmail.com>
2017-12-25 14:27:11 -0500

    validar usuario repetido

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -24,44 +24,67 @@ function saveUser (req, res){
         var _user = new User();
         _user.name = params.name;
         _user.surname = params.surname;
-        _user.nick = params.nick;
-        _user.email = params.email;
+        _user.nick = params.nick.toLowerCase();
+        _user.email = params.email.toLowerCase();
         _user.role = 'ROLE_USER';
         _user.image = null;
 
-        bcrypt.hash(params.password, null, null,
-        (err, hash) => {
-            _user.password = hash;
-
-            if(err){
-                return res.status(500)
-                .send({
-                    message: res.__('error.save.user')
-                });
-            }
-
-            _user.save(
-            (err, userStored) => {
-                if(err){
+        //Buscar si el usuario existe
+        User.findOne({
+            "$or": [
+                { email: _user.email },
+                { nick: _user.nick }
+            ]
+        }).exec(
+            (err, users) => {
+                if (err)
                     return res.status(500)
-                    .send({
-                        message: res.__('error.save.user')
-                    });
-                }
-                if(userStored){
-                    return res.status(200)
-                    .send({
-                        user: userStored
-                    });
-                }else{
-                    return res.status(404)
                     .send({
                         message: res.__('error.add.user')
                     });
-                }
-            });
-        })
+                
+                if(users){
+                    return res.status(500)
+                        .send({
+                            message: res.__('error.user.exist')
+                        });
+                }else{
 
+                    //Encriptar contraseña
+                    bcrypt.hash(params.password, null, null,
+                    (err, hash) => {
+                        _user.password = hash;
+
+                        if(err){
+                            return res.status(500)
+                            .send({
+                                message: res.__('error.save.user')
+                            });
+                        }
+
+                        _user.save(
+                        (err, userStored) => {
+                            if(err){
+                                return res.status(500)
+                                .send({
+                                    message: res.__('error.save.user')
+                                });
+                            }
+                            if(userStored){
+                                return res.status(200)
+                                .send({
+                                    user: userStored
+                                });
+                            }else{
+                                return res.status(404)
+                                .send({
+                                    message: res.__('error.add.user')
+                                });
+                            }
+                        });
+                    })                    
+                }
+        });
     }else{
         return res.status(200)
         .send({
