0b416fe21278768e8edc290638d4c20c34612c6d
mitarcon <mitarcon@gmail.com>
2017-12-27 22:24:43 -0500

    return image

--- api/routes/user.js
+++ api/routes/user.js
@@ -7,7 +7,7 @@ var md_auth = require('../middelwares/authenticated');
 var api = express.Router();
 
 var multipart = require('connect-multiparty');
-var md_upload = multipart({ uploadDir: './uploads/users'});
+var md_upload = multipart({ uploadDir: UserController.urlUserImg});
 
 // api.get('/user', md_auth.ensureAuth, UserController.home);
 api.get('/user', md_auth.ensureAuth, UserController.getUsers);
@@ -16,6 +16,7 @@ api.put('/user/:id', md_auth.ensureAuth, UserController.updateUser);
 api.put('/user/:id/upload/image', [md_auth.ensureAuth, md_upload], UserController.uploadImage);
 api.get('/user/:id', md_auth.ensureAuth, UserController.getUser);
 api.post('/user/action/login', UserController.loginUser);
+api.get('/user/action/image/:imageFile', UserController.getUserImage);
 
 
 module.exports = api;
