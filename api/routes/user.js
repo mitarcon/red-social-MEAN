0fb3431f457b479c24efd38c4bed441be5f8bfa9
mitarcon <mitarcon@gmail.com>
2017-12-27 20:55:08 -0500

    carga de imagenes

--- api/routes/user.js
+++ api/routes/user.js
@@ -6,10 +6,14 @@ var md_auth = require('../middelwares/authenticated');
 
 var api = express.Router();
 
+var multipart = require('connect-multiparty');
+var md_upload = multipart({ uploadDir: './uploads/users'});
+
 // api.get('/user', md_auth.ensureAuth, UserController.home);
 api.get('/user', md_auth.ensureAuth, UserController.getUsers);
 api.post('/user',  md_auth.ensureAuth, UserController.saveUser);
 api.put('/user/:id', md_auth.ensureAuth, UserController.updateUser);
+api.put('/user/:id/upload/image', [md_auth.ensureAuth, md_upload], UserController.uploadImage);
 api.get('/user/:id', md_auth.ensureAuth, UserController.getUser);
 api.post('/user/action/login', UserController.loginUser);
 
