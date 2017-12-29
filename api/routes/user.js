ad9bc90eb4093a4c37e58796d0912570b065797d
mitarcon <mitarcon@gmail.com>
2017-12-28 20:36:57 -0500

    cambio de url de user en app

--- api/routes/user.js
+++ api/routes/user.js
@@ -10,13 +10,13 @@ var multipart = require('connect-multiparty');
 var md_upload = multipart({ uploadDir: UserController.urlUserImg});
 
 // api.get('/user', md_auth.ensureAuth, UserController.home);
-api.get('/user', md_auth.ensureAuth, UserController.getUsers);
-api.post('/user',  md_auth.ensureAuth, UserController.saveUser);
-api.put('/user/:id', md_auth.ensureAuth, UserController.updateUser);
-api.put('/user/:id/upload/image', [md_auth.ensureAuth, md_upload], UserController.uploadImage);
-api.get('/user/:id', md_auth.ensureAuth, UserController.getUser);
-api.post('/user/action/login', UserController.loginUser);
-api.get('/user/action/image/:imageFile', UserController.getUserImage);
+api.get('', md_auth.ensureAuth, UserController.getUsers);
+api.post('',  md_auth.ensureAuth, UserController.saveUser);
+api.get('/:id', md_auth.ensureAuth, UserController.getUser);
+api.put('/:id', md_auth.ensureAuth, UserController.updateUser);
+api.put('/:id/upload/image', [md_auth.ensureAuth, md_upload], UserController.uploadImage);
+api.post('/action/login', UserController.loginUser);
+api.get('/action/image/:imageFile', UserController.getUserImage);
 
 
 module.exports = api;
